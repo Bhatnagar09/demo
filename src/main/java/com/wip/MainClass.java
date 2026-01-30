76f2effee51fd3ff1acb177bb6670cae5df71a6a
Pranav Bhatnagar <pranav26bhatnagar@gmail.com>
2026-01-30 12:18:12 +0530

    Fix typo in while loop condition

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -12,7 +12,7 @@ public class MainClass {
 		System.out.println("Java For Docker!!!! . THis code is pulled from WIP GitHub Repository");
 		System.out.println("This is the updated code after adding Jenkinsfile to the repository");
 
-		while(ture)
+		while(true)
 			{
 				try
 					{
